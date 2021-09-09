03b54c25d5fbb2f8b12eccb4d1e6e062310b1afa
Arnaldo Pagani <arnaldopaganijr@gmail.com>
2021-09-08 21:17:49 -0300

    adicionando variaveis

--- src/Disciplinas.java
+++ src/Disciplinas.java
@@ -5,15 +5,18 @@ public class Disciplinas extends Curso {
     private int nTurmas;
     private String bloco;
     private String horario;
+    private double [] notas = new double[3] ;
+    private double media;
 
-    public Disciplinas(int codCurse, String nome, String areaDeEnsino, String nomeDisciplina, int nTurmas, String bloco, String horario) {
-        super(codCurse, nome, areaDeEnsino);
-        this.nomeDisciplina = getNomeDisciplina();
+    public Disciplinas(int codCurso, String nome, String areaDeEnsino, String nomeDisciplina, int nTurmas, String bloco, String horario, double[] notas, double media) {
+        super(codCurso, nome, areaDeEnsino);
+        this.nomeDisciplina = nomeDisciplina;
         this.nTurmas = nTurmas;
         this.bloco = bloco;
         this.horario = horario;
+        this.notas = notas;
+        this.media = media;
     }
-
 
     public String getNomeDisciplina() {
         return nomeDisciplina;
@@ -45,5 +48,21 @@ public class Disciplinas extends Curso {
 
     public void setHorario(String horario) {
         this.horario = horario;
+    }
+
+    public double[] getNotas() {
+        return notas;
+    }
+
+    public void setNotas(double[] notas) {
+        this.notas = notas;
+    }
+
+    public double getMedia() {
+        return media;
+    }
+
+    public void setMedia(double media) {
+        this.media = media;
     }
 }
